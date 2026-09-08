8eb9ac095bce318f2d98fdbb9c54bcf80bc8b8a3
Abdenasser Farkli <abdenasserfarkli@gmail.com>
2026-09-08 16:36:05 +0200

    guides

---
+++ day01/guides/guide-01.js
@@ -0,0 +1,19 @@
+/**
+ * JOUR 01 — GUIDE 01
+ * Le Permutateur (Variables)
+ *
+ * OBJECTIF
+ * Vous avez deux variables a = 5 et b = 10. Vous devez échanger leurs valeurs pour que a vaille 10 et b vaille 5.
+ *
+ * Aide : consulte ../02-guides.md seulement si tu bloques.
+ */
+'use strict';
+
+let a= 5 , b = 10 , box = 0
+box=a
+a=b
+b=box
+console.log(a);
+console.log(b);
+
+
