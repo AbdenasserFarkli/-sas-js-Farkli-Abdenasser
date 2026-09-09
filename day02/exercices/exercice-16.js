ec866fc2fe12f5d697f30ab19cc72afcd91dea5e
Abdenasser Farkli <abdenasserfarkli@gmail.com>
2026-09-09 16:49:12 +0100

    exercice

---
+++ day02/exercices/exercice-16.js
@@ -0,0 +1,39 @@
+/**
+ * ─────────────────────────────────────────────────────────────
+ * JOUR 02 · EXERCICE 16 · NIVEAU 3 : DÉFI (AVANCÉS)
+ * SAPIN CENTRÉ
+ * ─────────────────────────────────────────────────────────────
+ *
+ * 🎯 MISSION
+ * Allez plus loin, dessinez une pyramide centrée de 5 lignes :
+ *
+ *     *
+ *    *
+ *   *
+ *  *
+ * 
+ *
+ * 📖 Consigne détaillée : ../03-exercices.md#exercice-16
+ * ▶️ Commande : node day02/exercices/exercice-16.js
+ */
+'use strict';
+
+// 1. Identifie les données nécessaires.
+// 2. Écris ta solution sous cette ligne.
+let base = ""
+for (let i = 1; i <= 5; i++) {
+    for (let j = 1; j<= 5; j++) {
+     for (let k = 1; k <= 9; k++) {
+            base += "*"
+        }
+        
+        
+    }
+        base += "\n"
+
+    
+}
+
+
+
+console.log(base);
