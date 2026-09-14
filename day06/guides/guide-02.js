8bc23aeaaf7c5de9e5dd4d0c917d20fabe3112ff
Abdenasser Farkli <abdenasserfarkli@gmail.com>
2026-09-14 16:59:03 +0100

    guides

---
+++ day06/guides/guide-02.js
@@ -0,0 +1,19 @@
+/**
+ * JOUR 06 — GUIDE 02
+ * Parcourir avec for...of
+ *
+ * OBJECTIF
+ * Vous avez notes = [12, 15, 8, 19]. Calculez la moyenne des notes en parcourant le tableau.
+ *
+ * Aide : consulte ../02-guides.md seulement si tu bloques.
+ */
+'use strict';
+
+ let table = [12, 15, 8, 19]
+ let sum =0
+ for (let i = 0; i < table.length; i++) {
+    sum+=table[i]
+    
+ }
+ console.log(sum/4);
+ 
